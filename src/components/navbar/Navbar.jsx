2d48ada0260d00537daee9b3056505d2876f492d
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-04-08 22:23:25 +0100

    Add Navbar component with navigation links; update dependencies in package.json

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-
-        <nav className="text">
-
+        <nav className="text-xl bg-slate-500" aria-label="Main Navigation">
+            <ul className="flex p-4 space-x-8 m-5">
+                <li>
+                    <Link
+                        to="/home"
+                        className="text-white hover:text-gray-300 transition-colors"
+                    >
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/Annonces"
+                        className="text-white hover:text-gray-300 transition-colors"
+                    >
+                        Annonces
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/login"
+                        className="text-white hover:text-gray-300 transition-colors"
+                    >
+                        Login
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/register"
+                        className="text-white hover:text-gray-300 transition-colors"
+                    >
+                        Register
+                    </Link>
+                </li>
+            </ul>
         </nav>
     );
 }
