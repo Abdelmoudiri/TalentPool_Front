114acdb199f9f71fdf4f32ca83e8be0eeacb8ef8
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-04-08 16:38:42 +0100

    Add Register component and integrate it into App; create Login component stub

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Register from './components/auth/Register';
 
 function App() {
 
   return (
     <>
+    <Register />
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
